22a3ed220220ac054589a0ee9f40f0c9cf2019c2
Jungyouyoung95 <ahrvhska200@gmail.com>
2022-04-21 15:36:28 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function percent(){
+    let num1 = parseInt(prompt("전체 숫자는?"));
+    let num2 = parseInt(prompt("일부 숫자는?"));
+    let result = ((num2/num1)*100);
+    alert(result + "% 입니다.");
+}
+
